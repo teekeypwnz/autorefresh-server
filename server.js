aa3466363610f4fb01b0489256a612b10aeae01e
teekeypwnz <teekeymr@gmail.com>
2026-03-23 01:10:44 +0300

    server.js

--- server.js
+++ server.js
@@ -148,4 +148,4 @@ if (type === "receive") {
 });
 
 // ================= SERVER =================
-app.listen(3000, () => console.log("Server started on port 3000"));
+app.listen(3000, () => console.log("Server started on port 10000"));
